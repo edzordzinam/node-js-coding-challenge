4ba7fa14600b5126930e3e3f5a1ea99e851b050b
Elvis Edzordzinam Dagadu <developer@edzordzinam.me>
2021-05-04 02:12:11 +0000

    Final Passed Test Cases

--- app.js
+++ app.js
@@ -1,27 +1,66 @@
 'use strict';
 
+//loading of configuration first
+const nconf = require('nconf');
+
+const config = nconf.argv()
+    .env()
+    .file({file: './config.json'});
+
 const express = require('express');
 const app = express();
 const http = require('http')
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-//initilaizing routes
-const routes = require('./routes')(app)
+//assigning config object to app object
+app.config = config;
 
-//initial server configurations
-const port = 3000
-const hostname = '127.0.0.1'
+const UserModel = require('./models/user')(mongoose)
+const AccessTokenModel = require('./models/accesstoken')(mongoose)
 
-//setup express
-//define routes
+//secret for session
+const session = require('express-session')({
+    secret: 'bofolekOSl',
+    resave: false,
+    saveUninitialized: false
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//initialize strategies and routes
+require('./passport/strategies')(app, passport, UserModel, AccessTokenModel)
+require('./routes')(app, passport, UserModel, AccessTokenModel)
 
 //client app entry point and serving of static libraries
 app.use(express.static(path.join(__dirname, 'app_client')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//start server
-http.createServer(app).listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+(async () => {
+    try {
+
+        await mongoose.connect(app.config.get('mongoose:uri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log('MongoDB successfully connected')
+
+        //now start server after successful db initialization
+        await http.createServer(app).listen(app.config.get('port'), app.config.get('hostname'));
+        console.log(`Server running at http://${app.config.get('hostname')}:${app.config.get('port')}/`);
+
+    } catch (error) {
+        //console.log(error.message)
+        process.exit(error)
+    }
+})();
 
 module.exports = app;
